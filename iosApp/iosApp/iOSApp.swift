4e3ced66bf9d12f61afeffc5d2f55bdd773c595e
“GaboDev98” <developer.gabo@gmail.com>
2024-11-07 18:01:14 -0500

    feat: firebase init ios

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,7 +1,13 @@
 import SwiftUI
+import ComposeApp
 
 @main
 struct iOSApp: App {
+
+    init() {
+        MainControllerKt.initialise()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
